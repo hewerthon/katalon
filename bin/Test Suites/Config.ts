382f844902c75ca3a74cbffbfc47ef89f90872d7
Hewerthon Adalberto <hewerthon@outlook.com>
2019-02-13 17:41:30 +0000

    Modificação de pastas

---
+++ bin/Test Suites/Config.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>Config</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Configuracoes</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
---
+++ bin/Test Suites/Mensagens.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Mensagens</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-14T13:49:15</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>07a323d9-7472-4d5b-aa7e-c581d134d7c5</testSuiteGuid>
+   <testCaseLink>
+      <guid>86a072f7-05bf-4c1e-94b8-fa82cb8a091b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mensagens/Mensagem</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
